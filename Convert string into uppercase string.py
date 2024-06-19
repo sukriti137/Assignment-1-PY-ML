91b49315f1d9734dcdce2f453c2067879822be1b
Sukriti137 <sukriti2004kumari@gmailcom>
2024-06-19 20:27:28 +0530

    First Commit

---
+++ Convert string into uppercase string.py
@@ -0,0 +1,12 @@
+def main():
+    # Get input string from user
+    input_string = input("Enter a string: ")
+    
+    # Convert the string to uppercase
+    uppercase_string = input_string.upper()
+    
+    # Print the uppercase string
+    print(f"The string in uppercase is: {uppercase_string}")
+
+if __name__ == "__main__":
+    main()
