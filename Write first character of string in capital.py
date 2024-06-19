d8cc97b6f328103acfcbe6fb4d7d7312b6323d6f
Sukriti137 <sukriti2004kumari@gmailcom>
2024-06-19 20:55:54 +0530

    First Commit

---
+++ Write first character of string in capital.py
@@ -0,0 +1,13 @@
+def to_title_case(input_string):
+    # Convert the input string to title case using str.title()
+    title_cased_string = input_string.title()
+    return title_cased_string
+
+# Input string
+input_string = "this is an example string to be converted to title case"
+
+# Convert to title case
+title_cased_string = to_title_case(input_string)
+
+# Print the result
+print(title_cased_string)
